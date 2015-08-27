9d5fc3813481a7932520caf3b8843c7bb29f2fff
artstepanyuk <artstepsoftserve@gmail.com>
2015-08-27 14:54:13 +0300

    offer.html edit logic ands style

--- src/client/app/offers/offers.js
+++ src/client/app/offers/offers.js
@@ -1,19 +1,39 @@
-﻿(function () {
+(function () {
     'use strict';
 
     angular
         .module('app.offers')
         .controller('OffersController', OffersController);
 
-    OffersController.$inject = ['$location'];
+    OffersController.$inject = ['$scope', '$location'];
 
-    function OffersController($location) {
+    function OffersController($scope, $location) {
         /* jshint validthis:true */
         var vm = this;
         vm.title = 'OffersController';
 
         activate();
 
-        function activate() { }
-    }
+        function activate() {
+          $scope.offerText = "";
+          $scope.editorEnabled = false;
+          
+          $scope.enableEditor = function() {
+            $scope.editorEnabled = true;
+            $scope.editableOffer = $scope.offerText;
+          };
+          
+          $scope.disableEditor = function() {
+            $scope.editorEnabled = false;
+          };
+          
+          $scope.save = function() {
+            $scope.offerText = $scope.editableOffer;
+            $scope.disableEditor();
+          };
+        };
+    };
 })();
+
+
+ 
