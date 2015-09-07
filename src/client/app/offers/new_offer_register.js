bd9e1b27439c55f3537ff55d8e7b6328bf505911
ypasch <tekilla.com@gmail.com>
2015-09-07 16:57:13 +0300

    offer logic

--- src/client/app/offers/new_offer_register.js
+++ src/client/app/offers/new_offer_register.js
@@ -5,15 +5,20 @@
         .module('app.offers')
         .controller('NewOfferRegisterController', NewOfferRegisterController);
 
-    NewOfferRegisterController.$inject = ['$location'];
+    NewOfferRegisterController.$inject = ['$location', '$scope'];
 
-    function NewOfferRegisterController($location) {
-        /* jshint validthis:true */
+    function NewOfferRegisterController($location, $scope) {
+
         var vm = this;
-        vm.title = 'NewOfferRegisterController';
+//        vm.title = 'NewOfferRegisterController';
 
         activate();
 
-        function activate() { }
-    }
+        vm.offer = {};
+        vm.saveData = function(){
+            console.log(vm.address);
+        };
+
+        function activate() {};
+    };
 })();
