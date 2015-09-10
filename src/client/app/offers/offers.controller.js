d60bf2c2451b8c9a78fcced250e1a821ced326ee
artstepanyuk <artstepsoftserve@gmail.com>
2015-09-10 15:51:16 +0300

    semantic names for controllers

--- src/client/app/offers/offers.controller.js
+++ src/client/app/offers/offers.controller.js
@@ -1,79 +1,40 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('app.offers')
-        .controller('EditOffersContoller', EditOffersContoller);
+        .controller('OffersController', OffersController);
 
-    EditOffersContoller.$inject = ['$location', '$filter', '$http', '$state'];
+    OffersController.$inject = ['$location', 'OffersFactory'];
 
-    /* @ngInject */
-    function EditOffersContoller($location, $filter, $http, $state) {
+    function OffersController($location, OffersFactory) {
+        /* jshint validthis:true */
         var vm = this;
-        vm.title = 'editOffersContoller';
-        vm.saveUser = saveUser;
-        vm.user = {};
-        /*Memo: Cities with Regions are hardcoded we will get them from backend in future */
-        vm.regions = [{
-            value: "Івано-Франківська область",
-            text: 'Івано-Франківська область'
-        }, {
-            value: "Київська область‎",
-            text: 'Київська область‎'
-        }, ];
-
-        vm.city = [{
-            value: "Івано-Франківськ",
-            text: ' Івано-Франківськ'
-        }, {
-            value: "Київ",
-            text: 'Київ'
-        }, ];
-        /* End of Memo*/
-        vm.user.size = 'S';
-        vm.size = [{
-            value: "S",
-            text: 'S'
-        }, {
-            value: "M",
-            text: 'M'
-        }, {
-            value: "L",
-            text: 'L'
-        }, {
-            value: "XL",
-            text: 'XL'
-        }, ];
-
-        vm.user.offer = "test title";
-        vm.user.offerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia eum quos distinctio, aspernatur ut porro. Voluptatibus culpa cumque distinctio deleniti, nam itaque accusamus ipsum vero illo eligendi voluptates velit consequuntur facere eaque asperiores minus fugit, debitis, nisi officia, maiores optio quos. Ut rerum cumque porro recusandae at, natus facilis quas!";
-        vm.user.size = "S";
-        vm.user.regions = "Київська область‎";
-        vm.user.city = "Київ";
-        vm.user.flat = "Номер квартири";
-        vm.user.time = "Пн-Пт, 10:00 - 19:00";
-        vm.user.street = "вул. Тараса Шевченка";
-        vm.user.house = "Номер будинку";
-
-        vm.user.status = 0;
-        vm.userStatuses = [{
-            value: 1,
-            text: "Зможу забрати"
-        }, {
-            value: 0,
-            text: "Не зможу забрати"
-        }];
+        vm.title = 'OffersController';
+        vm.enableEditor = enableEditor;
+        vm.disableEditor = disableEditor;
+        vm.save = save;
 
         activate();
 
-        function activate() {};
-
-        function saveUser() {
-            /*      return $http.post('/saveUser', vm.user).error(function(err) {
-                    console.log(vm.user); -----Logic for updating info on backend
-                    });*/
-            console.log(vm.user); /*edited info testing*/
+        function activate() {
+            vm.offerText = "";
+            vm.editorEnabled = false;
+            vm.data = OffersFactory.getOffers();
         };
 
-    }
+        function enableEditor() {
+            vm.editorEnabled = true;
+            vm.editableOffer = vm.offerText;
+        };
+
+        function disableEditor() {
+            vm.editorEnabled = false;
+        };
+
+        function save() {
+            vm.offerText = vm.editableOffer;
+            vm.disableEditor();
+        };
+    };
 })();
