bff6a630aa9cb78b50ebe3d193ddf39955d46202
Andrii Oleksyshyn <orphy@ukr.net>
2015-09-11 14:49:06 +0300

    unit tests

--- src/client/app/offers/offers.controller.js
+++ src/client/app/offers/offers.controller.js
@@ -11,30 +11,11 @@
         /* jshint validthis:true */
         var vm = this;
         vm.title = 'OffersController';
-        vm.enableEditor = enableEditor;
-        vm.disableEditor = disableEditor;
-        vm.save = save;
 
         activate();
 
         function activate() {
-            vm.offerText = "";
-            vm.editorEnabled = false;
             vm.data = OffersFactory.getOffers();
-        };
-
-        function enableEditor() {
-            vm.editorEnabled = true;
-            vm.editableOffer = vm.offerText;
-        };
-
-        function disableEditor() {
-            vm.editorEnabled = false;
-        };
-
-        function save() {
-            vm.offerText = vm.editableOffer;
-            vm.disableEditor();
         };
     };
 })();
