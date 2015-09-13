eeb042f0c7783e1f5d1f7f0e7b41ff08e773ea21
ArtStepanyuk <artstepsoftserve@gmail.com>
2015-09-13 21:30:22 +0300

    unit tests

---
+++ src/client/app/core/status/test/status.cheker.spec.js
@@ -0,0 +1,38 @@
+describe('statusCtrl', function() {
+    var controller;
+
+    beforeEach(function() {
+        controller = undefined;
+        scope = undefined;
+    });
+
+    beforeEach(function() {
+        module('app.core', 'app.offers');
+    });
+
+    beforeEach(function () {
+        bard.inject(this, '$controller', '$rootScope');
+        scope = $rootScope.$new();
+        controller = $controller('statusCtrl', {
+            $scope: scope
+        });
+        $rootScope.$apply();
+    });
+
+    describe('statusCtrl', function() {
+        before(function() {
+            console.log("testing of statusCtrl controller");
+        });
+        it('should be created successfully', function () {
+            expect(controller).to.be.defined;
+        });
+        it('should be a function', function () {
+            var vm = controller;
+            expect(vm.showStatus).to.be.a('function');
+        });
+        it('status should say undefined initially, until user clicks on edit button', function () {
+            var vm = controller;
+            expect(vm.status).to.be.an('undefined');
+        });
+    });
+});
---
+++ src/client/app/offers/tests/offers.edit.spec.js
@@ -0,0 +1,39 @@
+describe('EditOffersContoller', function() {
+    var controller;
+    beforeEach(function() {
+        controller = undefined;
+    });
+
+    beforeEach(function() {
+        module('app.core', 'app.offers');
+    });
+
+    beforeEach(function () {
+        bard.inject(this, '$controller', '$rootScope');
+        controller = $controller('EditOffersContoller', {
+        });
+    });
+
+    describe('EditOffersContoller', function() {
+        before(function() {
+            console.log("test of EditOffersContoller controller");
+        });
+        it('should be created successfully', function () {
+            expect(controller).to.be.defined;
+        });
+
+        it('should have name', function () {
+            var vm = controller;
+            expect(vm.user.offer).to.equal("test title");
+        });
+        it('should be a function', function () {
+            var vm = controller;
+            expect(vm.saveUser).to.be.a('function');
+        });
+        it('should have object with information from other page we edit', function () {
+            var vm = controller;
+            expect(vm.user).to.be.an('object');
+            expect(vm.need).to.be.empty;
+        });
+    });
+});
