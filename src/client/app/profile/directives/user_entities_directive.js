6e7940bfe56f84550bc7f35aaa92ba0e00ae7c35
asenyk <bloody.junior@gmail.com>
2015-09-14 11:43:56 +0300

    move profile to new folder

--- src/client/app/profile/directives/user_entities_directive.js
+++ src/client/app/profile/directives/user_entities_directive.js
@@ -1,24 +1,24 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('app.main').directive('userEntities', [
-        function() {
+        function () {
             return {
                 restrict: 'E',
-                template: '<ul><li ng-repeat="entiti in vm.list | limitTo: vm.limit">{{entiti}}</li><li ng-click="vm.toggleList()"><span ng-show="vm.list.length > vm.limit">[show more]</span><span ng-show="vm.list.length == vm.limit">[hide more]</span></ul>',
+                template: '<ul><li ng-show="vm.list.length === 0">No data</li><li ng-repeat="entiti in vm.list | limitTo: vm.limit">{{entiti}}</li><li ng-click="vm.toggleList()"><span ng-show="vm.list.length > vm.limit">[show more]</span><span ng-show="vm.list.length == vm.limit">[hide more]</span></ul>',
                 scope: {
                     list: '=',
                     limit: '=?'
                 },
-                controller: function() {
-                    if(!this.limit) {
+                controller: function () {
+                    if (!this.limit) {
                         this.limit = 1;
                     }
 
                     var limit = this.limit;
 
-                    this.toggleList = function() {
-                        if(this.limit < this.list.length) {
+                    this.toggleList = function () {
+                        if (this.limit < this.list.length) {
                             this.limit = this.list.length;
                         } else {
                             this.limit = limit;
