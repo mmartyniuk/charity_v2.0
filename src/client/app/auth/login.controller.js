99939dbf9e659ab1e4893b8c8f164719107036c4
chamer <chamer2010@gmail.com>
2015-09-22 17:53:33 +0300

    saved progress for further work

--- src/client/app/auth/login.controller.js
+++ src/client/app/auth/login.controller.js
@@ -5,16 +5,24 @@
         .module('app.auth')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['$http'];
+    LoginController.$inject = ['$http', '$window'];
 
-    function LoginController($http) {
+    function LoginController($http, $window) {
         var vm = this;
         vm.user = {};
+        vm.token = [];
         vm.authenticateUser = function () {
             $http.post('/api/auth', {username: vm.user.login, password: vm.user.password})
-            .success(function (response) {
-                console.log(response);
-            });
+                .success(function (response, headers, config, data) {
+                    console.log(data);
+                    console.log(response);
+                    console.log(headers);
+                    console.log(config);
+                    $http.get('/api/users/current', data)
+                        .success(function (response) {
+                            //console.log(response);
+                        });
+                });
         }
     }
 })();
