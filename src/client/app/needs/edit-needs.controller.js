03e2ea79483f4ac1796f103e6162ed11bbafca7c
artstepanyuk <artstepsoftserve@gmail.com>
2015-10-12 12:50:08 +0300

    date update

--- src/client/app/needs/edit-needs.controller.js
+++ src/client/app/needs/edit-needs.controller.js
@@ -18,7 +18,6 @@
         vm.currentNeed = {};
         vm.editedNeed = {};
         vm.date = {};
-
         vm.editedNeed.status = 0;
         vm.editedNeedStatuses = [{
             value: 1,
@@ -34,6 +33,7 @@
                 vm.editedNeed.needText = response.data.description;
                 vm.editedNeed.address = response.data.address;
                 vm.editedNeed.convenientTime = response.data.convenientTime;
+                vm.editedNeed.date = response.data.formattedActualTo;
             }).catch(function () {
                 console.log('something wrong');
             });
@@ -65,7 +65,8 @@
                 'description': vm.editedNeed.needText,
                 'address': vm.editedNeed.address,
                 'convenientTime': vm.editedNeed.convenientTime,
-                'pickup': vm.editedNeed.status
+                'pickup': vm.editedNeed.status,
+                'formattedActualTo': vm.editedNeed.date
 
             }).then(function () {
                 $state.go('needs.created', {id: needId});
