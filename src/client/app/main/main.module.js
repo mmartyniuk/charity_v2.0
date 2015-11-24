1798e953d9f3cea0bcf120b60a8dfbb291cb9894
a13ks3y <a13ks3y.ass@gmail.com>
2015-11-24 13:55:07 +0200

    translate placeholder

--- src/client/app/main/main.module.js
+++ src/client/app/main/main.module.js
@@ -3,5 +3,20 @@
 
     angular.module('app.main', [
 
-    ]);
+    ]).config(config);
+
+    config.$inject = ['$translateProvider'];
+
+    function config($translateProvider) {
+        $translateProvider.translations('ua', {
+            main: {
+                'whatAreYouLookingFor': 'Що ви шукаєте...'
+            }
+        });
+        $translateProvider.translations('ru', {
+            main: {
+                'whatAreYouLookingFor': 'Что вы ищете...'
+            }
+        });
+    }
 })();
