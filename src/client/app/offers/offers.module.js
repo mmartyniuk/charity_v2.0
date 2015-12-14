73bf425a27b8be47abd5853374fc7a34cccce7fd
Ivanna Besarab <ivanna.besarab@cgm.com>
2015-12-14 15:55:14 +0200

    merged with upstream

--- src/client/app/offers/offers.module.js
+++ src/client/app/offers/offers.module.js
@@ -6,6 +6,63 @@
 
     function config($translateProvider) {
         $translateProvider.translations('ua', {
+            offers: {
+                offer: 'Пропозиція',
+                offers: 'Пропозиції',
+                author: 'Автор',
+                published: 'Опубліковано',
+                category: 'Категорія',
+                offerDescription: 'Опис пропозиції',
+                description: 'Опис',
+                address: 'Адреса',
+                convenientTime: 'Зручний час',
+                canTake: 'Зможу забрати',
+                edit: 'Редагувати',
+                close: 'Закрити',
+                delete: 'Видалити',
+                respond: 'Відгукнутись',
+                cancelResponse: 'Відмінити відгук',
+                details: 'Деталі',
+                typeDetailsPlaceholder: 'Введіть будь ласка деталі',
+                submit: 'Підтвердити',
+                userName: 'Ім\'я користувача',
+                phoneNumber: 'Телефон',
+                receive: 'Отримати',
+                confirmAssistance: 'Підтвердити отримання допомоги',
+                cancelGettingResponseFromUser: ' Відмовитись від допомоги цього користувача',
+                offerIsClosed: 'Ця пропозиція закрита',
+
+                editingOfOffer: 'Редагування пропозиції',
+                offerName: 'Назва',
+                save: 'Зберегти',
+
+                newOffer: 'Нова пропозиція',
+                nameOfOfferPlaceholder: 'Назва потреби',
+                selectCategoryPlaceholder: 'Виберіть категорію',
+                describeOfferPlaceholder: 'Опишіть вашу пропозицію',
+                photo: 'Фото',
+                selectRegion: 'Виберіть область',
+                selectCity: 'Виберіть місто',
+                dateTimePlaceholder: 'Пн-Пт, 10:00 - 19:00;',
+                publishButton: 'Опублікувати',
+                backButton: 'Назад',
+                furtherButton: 'Далі',
+                selectImageButton: 'Вибрати зображення',
+                uploadButton: 'Завантажити',
+                actuality: 'Актуальність',
+
+                display: 'Відображати',
+                firstPage: 'Перша',
+                lastPage: 'Остання',
+                previousPage: 'Попередня',
+                nextPage: 'Наступна',
+                lookingFor: 'Що ви шукаєте',
+
+                offersPaginationLabel: 'пропозицій',
+                allOffers: 'Усі пропозиції',
+                offersFound: 'За Вашим запитом знайдено пропозицій: {{count}}',
+                nothingFound: 'На жаль за Вашим запитом нічого не знайдено.'
+            },
             editOffer: {
                 editingOfOffer: 'Редагування пропозиції',
                 offerName: 'Назва',
@@ -46,78 +103,6 @@
                 confirmAssistance: 'Підтвердити отримання допомоги',
                 cancelAssistanceFromUser: 'Відмовитися від допомоги цього користувача',
                 offerIsClosed: 'Ця пропозиція закрита'
-            }
-        });
-        $translateProvider.translations('ru', {
-            editOffer: {
-                editingOfOffer: 'Редактирование предложения',
-                offerName: 'Название',
-                category: 'Категория',
-                offerDescription: 'Описание предложения',
-                photo: 'Фото',
-                selectRegion: 'Выберите область',
-                selectCity: 'Выберите город',
-                address: 'Адрес',
-                convenientTime: 'Удобное время',
-                edit: 'Редактировать',
-                save: 'Cохранить',
-                cancel: 'Отменить',
-                requiredField: 'Обязательное поле'
-            },
-            createdOffer: {
-                offer: 'Предложение',
-                author: 'Автор',
-                published: 'Опубликовано',
-                category: 'Категория',
-                offerDescription: 'Описание предложения',
-                description: 'Описание',
-                actuality: 'Актуальность',
-                address: 'Адрес',
-                convenientTime: 'Удобное время',
-                canTake: 'Смогу забрать',
-                edit: 'Редактировать',
-                close: 'Закрыть',
-                delete: 'Удалить',
-                respond: 'Откликнуться',
-                cancelResponse: 'Отменить отзыв',
-                details: 'Детали',
-                typeDetailsPlaceholder: 'Введите пожалуйста детали',
-                submit: 'Подтвердить',
-                userName: 'Имя пользователя',
-                phoneNumber: 'Телефон',
-                recieve: 'Получить',
-                confirmAssistance: 'Подтвердить получение помощи',
-                cancelAssistanceFromUser: 'Отказаться от помощи этого пользователя',
-                offerIsClosed: 'Это предложение закрытое'
-            }
-        })
-    }
-                newOffer: 'Нова пропозиція',
-                nameOfOfferPlaceholder: 'Назва потреби',
-                selectCategoryPlaceholder: 'Виберіть категорію',
-                describeOfferPlaceholder: 'Опишіть вашу пропозицію',
-                photo: 'Фото',
-                selectRegion: 'Виберіть область',
-                selectCity: 'Виберіть місто',
-                dateTimePlaceholder: 'Пн-Пт, 10:00 - 19:00;',
-                publishButton: 'Опублікувати',
-                backButton: 'Назад',
-                furtherButton: 'Далі',
-                selectImageButton: 'Вибрати зображення',
-                uploadButton: 'Завантажити',
-                actuality: 'Актуальність',
-
-                display: 'Відображати',
-                firstPage: 'Перша',
-                lastPage: 'Остання',
-                previousPage: 'Попередня',
-                nextPage: 'Наступна',
-                lookingFor: 'Що ви шукаєте',
-
-                offersPaginationLabel: 'пропозицій',
-                allOffers: 'Усі пропозиції',
-                offersFound: 'За Вашим запитом знайдено пропозицій: {{count}}',
-                nothingFound: 'На жаль за Вашим запитом нічого не знайдено.'
             }
         });
         $translateProvider.translations('ru', {
@@ -177,7 +162,48 @@
                 allOffers: 'Все предложения',
                 offersFound: 'По Вашему запросу найдено предложений: {{count}}',
                 nothingFound: 'К сожалению по вашему запросу ничего не найдено.'
+            },
+            editOffer: {
+                editingOfOffer: 'Редактирование предложения',
+                offerName: 'Название',
+                category: 'Категория',
+                offerDescription: 'Описание предложения',
+                photo: 'Фото',
+                selectRegion: 'Выберите область',
+                selectCity: 'Выберите город',
+                address: 'Адрес',
+                convenientTime: 'Удобное время',
+                edit: 'Редактировать',
+                save: 'Cохранить',
+                cancel: 'Отменить',
+                requiredField: 'Обязательное поле'
+            },
+            createdOffer: {
+                offer: 'Предложение',
+                author: 'Автор',
+                published: 'Опубликовано',
+                category: 'Категория',
+                offerDescription: 'Описание предложения',
+                description: 'Описание',
+                actuality: 'Актуальность',
+                address: 'Адрес',
+                convenientTime: 'Удобное время',
+                canTake: 'Смогу забрать',
+                edit: 'Редактировать',
+                close: 'Закрыть',
+                delete: 'Удалить',
+                respond: 'Откликнуться',
+                cancelResponse: 'Отменить отзыв',
+                details: 'Детали',
+                typeDetailsPlaceholder: 'Введите пожалуйста детали',
+                submit: 'Подтвердить',
+                userName: 'Имя пользователя',
+                phoneNumber: 'Телефон',
+                recieve: 'Получить',
+                confirmAssistance: 'Подтвердить получение помощи',
+                cancelAssistanceFromUser: 'Отказаться от помощи этого пользователя',
+                offerIsClosed: 'Это предложение закрытое'
             }
-        });
+        })
     }
 })();
